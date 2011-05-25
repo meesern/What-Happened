a8d53ae8f28b3ac6277e579769c828d0bcfa3726
Rupert Meese <rupert.meese@lightmind.co.uk>
2011-05-25 16:06:33 +0100

    Back out from :scm :git due to unknown instance problem.

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,13 +14,13 @@ end
 
 # Use a simple directory tree copy here to make demo easier.
 # You probably want to use your own repository for a real app
-#set :scm, :none
-#set :repository, "."
-#set :deploy_via, :copy
+set :scm, :none
+set :repository, "."
+set :deploy_via, :copy
 #NOTE Currently using git fails with "Instance not found for host production" since the instance-production.yml is not in git.
-set :scm, :git
-set :repository, "git://github.com/meesern/What-Happened.git"
-set :deploy_via, :remote_cache
+#set :scm, :git
+#set :repository, "git://github.com/meesern/What-Happened.git"
+#set :deploy_via, :remote_cache
 
 # Easier to do system level config as root - probably should do it through
 # sudo in the future.  We use ssh keys for access, so no passwd needed
